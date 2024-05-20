d531f711f85a3c41234d10da126c0790f8c31ec8
DennisSvenss0n <Dennizz632@gmail.com>
2024-05-20 21:34:08 +0200

    Added Business Component

---
+++ ravenous/src/Components/Business.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function Business(prop) {
+    return (
+        <div className='restaurantCard'>
+            <a href={prop.info.url} target='_blank' rel="noreferrer">
+                <img className='image' src={prop.info.img} alt='restaurant' />
+            </a>
+            <div className='info'>
+                <p className='name'>{prop.info.name}</p>
+                <p className='address'>{prop.info.address}</p>
+                <p className='city'>{prop.info.city}</p>
+                <p className='state'>{prop.info.state}</p>
+                <p className='zipcode'>{prop.info.zipcode}</p>
+                <p className='category'>{prop.info.category}</p>
+                <p className='rating'>{prop.info.rating}</p>
+                <p className='reviewCount'>{prop.info.reviewCount}</p>
+            </div>
+        </div>
+    );
+    
+}
+
+export default Business;
