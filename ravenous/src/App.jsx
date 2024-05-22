c0cf3d1db9f24a5d185454c0dd7034d7f72c4d44
DennisSvenss0n <Dennizz632@gmail.com>
2024-05-22 21:37:39 +0200

    modified App.jsx and added CSS files to Components

---
+++ ravenous/src/App.jsx
@@ -0,0 +1,12 @@
+import BusinessList from './Components/BusinessList/BusinessList';
+import SearchBar from './Components/SearchBar/SearchBar';
+
+function app () {
+    return (
+    <div>
+        <BusinessList />
+        <SearchBar />
+
+    </div>
+    );
+}
