fe933682cd8ba7e6a1347b8803f00a2cd1ed0596
DennisSvenss0n <Dennizz632@gmail.com>
2024-05-22 21:19:00 +0200

    Added bootstrap

---
+++ ravenous/src/Components/Business/Business.jsx
@@ -0,0 +1,14 @@
+const Business = {
+    imagesrc: 'https://f.nordiskemedier.dk/2h6v7min3g0torhb_660_368.png',
+    name: 'O´Learys',
+    address: 'Vasagatan',
+    city: 'Mora',
+    state: 'Dalarna',
+    zipcode: 79433,
+    category: 'Food',
+    rating: 4.5,
+    reviewCount: 24,
+
+};
+
+export default Business;
---
+++ ravenous/src/Components/BusinessList/BusinessList.jsx
@@ -0,0 +1,15 @@
+import Business from "../Business/Business";
+
+function BusinessList () {
+    return (
+        <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    );
+}
+
